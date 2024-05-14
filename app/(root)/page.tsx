266e54efbe7682d4026c415a74e6c25694b51790
Anuragp22 <anuragp2003b@gmail.com>
2024-05-15 00:12:04 +0530

    Final Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
@@ -22,7 +23,9 @@ const Home = () => {
                         totalBanks={1}
                         totalCurrentBalance={1250.35} />
                 </header>
+                Recent Transactions
             </div>
+            <RightSideBar user={loggedIn} transactions={[]} banks={[{}, {}]} />
         </section>
     )
 }
